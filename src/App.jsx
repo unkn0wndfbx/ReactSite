7e795f42f0c0500f74002213140b8e6eddd82129
unkn0wnd.fbx <titouan.helbert@gmail.com>
2024-09-20 17:45:05 +0200

    files add

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
+import Header from "./components/Header.jsx";
+import Hero from "./components/Hero.jsx";
+import Benefits from "./components/Benefits.jsx";
+import Collaboration from "./components/Collaboration.jsx";
+
+const App = () => {
+    return (
+        <>
+            <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+                <Header/>
+                <Hero/>
+                <Benefits/>
+                <Collaboration/>
+            </div>
+
+            <ButtonGradient/>
+        </>
+    );
+};
+
+export default App
